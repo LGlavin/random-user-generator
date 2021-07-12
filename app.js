d21f6ed0a5e978ff9eb8fb0948942cc7f48e212f
Liz Glavin <eglavin@gmail.com>
2021-07-11 16:15:30 -1000

    fixed style

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ const app = Vue.createApp({
         async getUser() {
             const res = await fetch('https://randomuser.me/api/')
             const {results} = await res.json()
-
-
-           this.firstName =  results[0].name.firstName
-           this.lastName = results[0].name.last          
+            this.firstName =  results[0].name.firstName
+            this.lastName = results[0].name.last          
             this.gender = results[0].gender
-            this.picture = results[0].picture.large        },
+            this.picture = results[0].picture.large        
+        },
     },
 })
 
